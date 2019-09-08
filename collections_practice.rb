ea9d798b806166ed5cd7acf98b765ab559beb25b
christian williams <christian.williamsj@gmail.com>
2019-09-08 20:15:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,8 +30,19 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  
-  
-  
-  
+  array.select do |x|
+    y = x.split("")
+    if y[0] == "a" || y[0] == "A"
+      true
+    end
+  end
+end
+
+ def sum_array(array)
+  array.inject(:+)
+end
+
+ def add_s(array)
+  array.each_with_index.collect { |x,y| y == 1 ? x : x << "s" }
+end 
   
